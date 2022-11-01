c1a803f8b8470ce4e43642a3abec240b3afe5419
John Doe <ramazonov3600@gmail.com>
2022-11-02 01:47:47 +0500

    finish project

--- main.js
+++ main.js
@@ -3,8 +3,12 @@ const elMoviesList = document.querySelector(".movies-list");
 const elMoviesTemplate = document.querySelector(".movies-temp").content;
 const MoviesFragment = document.createDocumentFragment();
 const elForm = document.querySelector(".site-form");
-const elFormInput = document.querySelector(".js-input");
 const elSelect = document.querySelector(".site-select");
+const elFormInput = document.querySelector(".js-input");
+const elSearchStart = document.querySelector(".form-search-start")
+const elSearchEnd = document.querySelector(".form-search-end")
+
+const elSelectSort = document.querySelector(".site-select-sort");
 
 
 // MODAL
@@ -20,12 +24,56 @@ const modalLink = elModal.querySelector(".modal-imdb-link");
 
 const categorySum = []
 
-function getDuration (time){
+const yearEndSum = []
+
+
+function sortFunc(arr, select){
+  if(select == "A-Z"){
+    arr.sort((a, b) => {
+      if(String(a.Title) > String(b.Title)){
+        return 1
+      }
+      else if(String(a.Title) < String(b.Title)){
+        return -1
+      }
+      else{
+        return 0
+      }
+    })
+  }
+  else if(select == "Z-A"){
+    arr.sort((a, b) =>{
+      if(String(a.Title) > String(b.Title)){
+        return -1
+      }
+      else if(String(a.Title) < String(b.Title)){
+        return 1
+      }
+      else{
+        return 0
+      }
+    })
+  }
+
+  if(select == "2000-2018"){
+    arr.sort((a, b) => a.movie_year - b.movie_year)
+  }
+  else if(select == "2018-2000"){
+    arr.sort((a, b) => b.movie_year - a.movie_year)
+  }
   
+  if(select == "1-10"){
+    arr.sort((a, b)=> a.imdb_rating - b.imdb_rating)
+  }
+  else if(select == "10-1"){
+    arr.sort((a, b)=> b.imdb_rating - a.imdb_rating)
+  }
+}
+
+function getDuration (time){
   const hours = Math.floor(time / 60 );
   const minuts = Math.floor(time % 60 );
   return `${hours} hrs ${minuts} min  `
-  
 }
 
 function renderMovies(kino){
@@ -33,21 +81,22 @@ function renderMovies(kino){
   
   kino.forEach(item => {
     const elCloneMovie = elMoviesTemplate.cloneNode(true);
-    item.Categories.split("|").forEach(function(element, index){
+    item.Categories.split("|").forEach(function(element){
       if(!categorySum.includes(element)){
         categorySum.push(element);
         const elOption = document.createElement("option");
         elOption.textContent = element;
         elSelect.appendChild(elOption);
-      }
+      } 
       
     })
+    
     elCloneMovie.querySelector(".movie-img").src = `https://i3.ytimg.com/vi/${item.ytid}/mqdefault.jpg `;
     elCloneMovie.querySelector(".movies-title").textContent = item.Title;
     elCloneMovie.querySelector(".movie-rating").textContent = item.imdb_rating;
     elCloneMovie.querySelector(".movie-year").textContent = item.movie_year;
     elCloneMovie.querySelector(".movie-runtime").textContent =  getDuration(item.runtime);
-    elCloneMovie.querySelector(".movie-categories").textContent = item.Categories.split("|").join("|");
+    elCloneMovie.querySelector(".movie-categories").textContent = item.Categories.split("|").join("| ");
     elCloneMovie.querySelector(".movie-btn").dataset.id = item.imdb_id;
     MoviesFragment.appendChild(elCloneMovie);
     
@@ -67,7 +116,6 @@ function renderModalInfo(topilganKino){
   modalLink.href = `https://www.imdb.com/title/${topilganKino.imdb_id}`;
 }
 
-// event delegation
 elMoviesList.addEventListener("click",function(evt){
   const targetElement = evt.target
   if(targetElement.matches(".movie-btn")){
@@ -85,19 +133,31 @@ elForm.addEventListener("submit", function(evt){
   evt.preventDefault();
   
   
-  
-  
+  const elSelctCatecoryValue = elSelect.value
   const elFormInputValue = elFormInput.value.trim();
   const elSelectValue = elSelect.value.trim();
-  
+  const elSearchStartValue = Number(elSearchStart.value)
+  const elSearchEndValue = Number(elSearchEnd.value)
   const regexValue = new RegExp(elFormInputValue, "gi");
+  const regexCategory = new RegExp(elSelctCatecoryValue, "gi"); 
+  const elSelectSortValue = elSelectSort.value;
   
-  const searchMovie = movies.filter(item => String(item.Title).match(regexValue) && elSelectValue === item.Categories || String(item.Title).match(regexValue) && elSelectValue === "All");
-  
-  if (searchMovie.length > 0){
-    renderMovies(searchMovie);
-  }else{
-    elMoviesList.innerHTML = "Not found !!!";
+  sortFunc(movies, elSelectSortValue);
+ 
+  if(elFormInputValue == ""){
+    const searchMovie = movies.filter(item => (item.Categories.match(regexCategory) || elSelctCatecoryValue === "All") && ((elSearchStartValue <= item.movie_year && elSearchEndValue >= item.movie_year) || (elSearchStartValue == "" && elSearchEndValue >= item.movie_year) || (elSearchStartValue <= item.movie_year && elSearchEndValue == "")));
+    renderMovies(searchMovie.slice(0));
+  }
+  else{
+    const searchMovie = movies.filter(item => String(item.Title).match(regexValue) && (elSelectValue === item.Categories || String(item.Title).match(regexValue) && elSelectValue === "All")  && ((elSearchStartValue <= item.movie_year && elSearchEndValue >= item.movie_year) || (elSearchStartValue == "" && elSearchEndValue >= item.movie_year) || (elSearchStartValue <= item.movie_year && elSearchEndValue == "")));
+    
+    renderMovies(searchMovie.slice(0));
+    
+    if (searchMovie.length > 0){
+      renderMovies(searchMovie);
+    }else{
+      elMoviesList.innerHTML = "Not found !!!";
+    }
   }
 })
 
